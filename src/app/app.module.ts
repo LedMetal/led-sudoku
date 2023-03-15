23f9db0c8451bb14e8cc491234e281ceb7488ac1
Abdul Sadiq <sadiq9707336@gmail.com>
2023-03-14 23:52:33 -0400

    include font awesome in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { SquareComponent } from './square/square.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BoardComponent,
-    SquareComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, BoardComponent, SquareComponent],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
